bc0e9328f24e9eff29b5936a3b2edb83cea26b4b
AllanSmithll <allanammancio2004@gmail.com>
2022-11-30 22:16:30 -0300

    Atualizações dos componentes

--- minhaestantereact/src/index.js
+++ minhaestantereact/src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Carrossel from './components/Carrossel.jsx';
+import Cabecalho from './components/Cabecalho.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <Cabecalho />
+    <Carrossel />
   </React.StrictMode>
 );
 
